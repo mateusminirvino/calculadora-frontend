f89ed93f5ffdf64535a88f91703e9a5627ab5cac
Mateus Minirvino <mateusminirvino2@gmail.com>
2018-12-14 16:23:13 -0200

    Copiado projeto da calculadora front

---
+++ calculadora-frontend/src/app/app.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+import{HttpClient} from '@angular/common/http'
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  numeroUm: number = 0;
+  numeroDois: number = 0;
+  operacao: string = 'SOMA';
+  response: any = {};
+
+  constructor(private http:HttpClient){
+  }
+
+  onClickExecutarOperacao(){
+    const calculo = {
+      operacao: this.operacao,
+      numeroUm: this.numeroUm,
+      numeroDois: this.numeroDois
+    };
+    this.http.post('http://localhost:9090/api/calculadora', calculo).subscribe(response => this.response = response);
+  }
+}
